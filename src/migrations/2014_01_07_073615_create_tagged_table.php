2bacc951eef47610d6973087d3f1d062f681aeb3
njitt <hlz@dds.nl>
2014-11-10 11:53:00 +0100

    changed taggable_id to int type

--- src/migrations/2014_01_07_073615_create_tagged_table.php
+++ src/migrations/2014_01_07_073615_create_tagged_table.php
@@ -8,7 +8,7 @@ class CreateTaggedTable extends Migration {
 	public function up() {
 		Schema::create('tagging_tagged', function(Blueprint $table) {
 			$table->increments('id');
-			$table->string('taggable_id', 36)->index();
+			$table->int('taggable_id')->index();
 			$table->string('taggable_type', 255)->index();
 			//$table->morphs('taggable');
 			$table->string('tag_name', 255);
